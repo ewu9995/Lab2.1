686be9ec9f64fdc3a561e53ee341308dcbd7abb7
Author <PC@Dell>
2017-11-08 19:29:13 -0500

    Lab 2.1 Shapes Inital commit

---
+++ Lab 2.1 Shapes/src/shapes/Circle.java
@@ -0,0 +1,39 @@
+package shapes;
+
+/**
+ * @author Mr Levin Created 10/16/2017 Lab 2.1 shapes
+ *
+ */
+public class Circle implements Shape {
+
+	// fields
+	private int radius;
+	private static double pi = 3.14;
+
+	/**
+	 * Constructor method
+	 * 
+	 * @param radius
+	 */
+	public Circle(int radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public double calculateArea() {
+		double area = (radius*radius*pi);
+		return area;
+	}
+
+	@Override
+	public double calculatePerimeter() {
+		double perimeter = (radius*2*pi);
+		return perimeter;
+	}
+
+	@Override
+	public String toString() {
+		return "Circle radius: " + radius + " Area: " + this.calculateArea() + " Perimeter: " + this.calculatePerimeter();
+	
+	}
+}
---
+++ Lab 2.1 Shapes/src/shapes/Rectangle.java
@@ -0,0 +1,34 @@
+package shapes;
+
+/**
+ * @author Mr Levin Created 10/16/2017 Lab 2.1 shapes
+ *
+ */
+public class Rectangle implements Shape {
+
+	private int length;
+	private int width;
+
+	public Rectangle(int length, int width) {
+		this.length = length;
+		this.width = width;
+	}
+
+
+	public double calculateArea() {
+		double area = length*width;
+		return area; 
+	}
+
+
+	public double calculatePerimeter() {
+		double perimeter = (2*length) + (2*width);
+		return perimeter;
+	}
+
+
+	public String toString() {
+		return "Rectangle Width: " + width + " Length: " + length + " Area: " + this.calculateArea() + " Perimeter: "
+				+ this.calculatePerimeter();
+	}
+}
