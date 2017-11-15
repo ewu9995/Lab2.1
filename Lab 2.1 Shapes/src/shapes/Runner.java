c50dd086020aa1ed3f92bdb21889c39d9f731a96
Author <PC@Dell>
2017-11-15 16:39:04 -0500

    fixes

--- Lab 2.1 Shapes/src/shapes/Runner.java
+++ Lab 2.1 Shapes/src/shapes/Runner.java
@@ -14,14 +14,14 @@ package shapes;
  *         Both classes must extend shape, and implement all necessary methods.
  */
 public class Runner {
-
+	//NOTE: Triangle is ONLY for REGULAR triangles and Trapezoid is ONLY for REGULAR trapezoids!
 	public static void main(String[] args) {
 		Shape circle1 = new Circle(3); //radius
 		Shape rectangle1 = new Rectangle(10, 4); //length, width
-		Shape square1 = new Square(10); //side legnth
-		Shape triangle1 = new Triangle(30,25,11); //base, side1, side2
-		Shape trapezoid1 = new Trapezoid(26,13,16,13); //length1(bottom base), width1(left side), length2(top base), width2(right side) 
-
+		Shape square1 = new Square(10); //side length
+		Shape triangle1 = new Triangle(72,76,52); // side1, base, side2  
+		Shape trapezoid1 = new Trapezoid(30,20,18,20); //length1(bottom base), width1(left side), length2(top base), width2(right side) 
+		
 		System.out.println("First test out the hard coded shape arraay.\n\n");
 		Shape[] shapeArr = { circle1, rectangle1, square1, triangle1, trapezoid1};
 
--- Lab 2.1 Shapes/src/shapes/Trapezoid.java
+++ Lab 2.1 Shapes/src/shapes/Trapezoid.java
@@ -8,7 +8,7 @@ public class Trapezoid implements Shape {
 	private int c;
 	private int d;
 	
-//only works for a regular trapezoid 
+	//only works for a regular trapezoid  
 	public Trapezoid(int length1, int width1, int length2, int width2) {
 		this.a = length1;
 		this.b = width1;
@@ -18,7 +18,7 @@ public class Trapezoid implements Shape {
 
 
 	public double calculateArea() {
-		int x = ((b*b)-(d*d)+(a-c)*(a-c))/(2*(a-c));
+		double x = ((b*b)-(d*d)+(a-c)*(a-c))/(2*(a-c));
 		double area = (((a+c)/2) * Math.sqrt((b*b) - (x*x)));
 		return area; 
 	}
